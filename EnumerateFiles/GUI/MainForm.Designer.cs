8b77aa88435157d0213970458172951a14100605
EmineBasheva <eminka23@abv.bg>
2016-06-25 17:11:13 +0300

    add basic logic

---
+++ EnumerateFiles/GUI/MainForm.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace GUI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBoxFilesDirectory = new System.Windows.Forms.TextBox();
+            this.buttonBrowse = new System.Windows.Forms.Button();
+            this.labelChoiceFolder = new System.Windows.Forms.Label();
+            this.progressBarEnumeration = new System.Windows.Forms.ProgressBar();
+            this.buttonEnumerate = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.labelSortBy = new System.Windows.Forms.Label();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // textBoxFilesDirectory
+            // 
+            this.textBoxFilesDirectory.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxFilesDirectory.Location = new System.Drawing.Point(12, 40);
+            this.textBoxFilesDirectory.Name = "textBoxFilesDirectory";
+            this.textBoxFilesDirectory.Size = new System.Drawing.Size(342, 22);
+            this.textBoxFilesDirectory.TabIndex = 0;
+            // 
+            // buttonBrowse
+            // 
+            this.buttonBrowse.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonBrowse.Location = new System.Drawing.Point(360, 38);
+            this.buttonBrowse.Name = "buttonBrowse";
+            this.buttonBrowse.Size = new System.Drawing.Size(107, 24);
+            this.buttonBrowse.TabIndex = 1;
+            this.buttonBrowse.Text = "Browse ...";
+            this.buttonBrowse.UseVisualStyleBackColor = true;
+            this.buttonBrowse.Click += new System.EventHandler(this.buttonBrowse_Click);
+            // 
+            // labelChoiceFolder
+            // 
+            this.labelChoiceFolder.AutoSize = true;
+            this.labelChoiceFolder.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelChoiceFolder.Location = new System.Drawing.Point(27, 13);
+            this.labelChoiceFolder.Name = "labelChoiceFolder";
+            this.labelChoiceFolder.Size = new System.Drawing.Size(142, 24);
+            this.labelChoiceFolder.TabIndex = 2;
+            this.labelChoiceFolder.Text = "Choice folder ...";
+            // 
+            // progressBarEnumeration
+            // 
+            this.progressBarEnumeration.Location = new System.Drawing.Point(12, 65);
+            this.progressBarEnumeration.Name = "progressBarEnumeration";
+            this.progressBarEnumeration.Size = new System.Drawing.Size(342, 5);
+            this.progressBarEnumeration.TabIndex = 3;
+            this.progressBarEnumeration.Visible = false;
+            // 
+            // buttonEnumerate
+            // 
+            this.buttonEnumerate.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonEnumerate.Location = new System.Drawing.Point(12, 80);
+            this.buttonEnumerate.Name = "buttonEnumerate";
+            this.buttonEnumerate.Size = new System.Drawing.Size(455, 35);
+            this.buttonEnumerate.TabIndex = 4;
+            this.buttonEnumerate.Text = "Enumerate";
+            this.buttonEnumerate.UseVisualStyleBackColor = true;
+            this.buttonEnumerate.Click += new System.EventHandler(this.buttonEnumerate_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "",
+            "Name",
+            "Size"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 101);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(226, 24);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.Visible = false;
+            // 
+            // labelSortBy
+            // 
+            this.labelSortBy.AutoSize = true;
+            this.labelSortBy.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSortBy.Location = new System.Drawing.Point(12, 80);
+            this.labelSortBy.Name = "labelSortBy";
+            this.labelSortBy.Size = new System.Drawing.Size(62, 16);
+            this.labelSortBy.TabIndex = 6;
+            this.labelSortBy.Text = "Sort by ...";
+            this.labelSortBy.Visible = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Enabled = false;
+            this.checkBox1.Location = new System.Drawing.Point(255, 85);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(76, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Ascending";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.Visible = false;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Enabled = false;
+            this.checkBox2.Location = new System.Drawing.Point(255, 108);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(83, 17);
+            this.checkBox2.TabIndex = 8;
+            this.checkBox2.Text = "Descending";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.Visible = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(479, 140);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.labelSortBy);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.buttonEnumerate);
+            this.Controls.Add(this.progressBarEnumeration);
+            this.Controls.Add(this.labelChoiceFolder);
+            this.Controls.Add(this.buttonBrowse);
+            this.Controls.Add(this.textBoxFilesDirectory);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Enumerate files";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBoxFilesDirectory;
+        private System.Windows.Forms.Button buttonBrowse;
+        private System.Windows.Forms.Label labelChoiceFolder;
+        private System.Windows.Forms.ProgressBar progressBarEnumeration;
+        private System.Windows.Forms.Button buttonEnumerate;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label labelSortBy;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ EnumerateFiles/GUI/MainForm.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class MainForm : Form
+    {
+        private string directoryPath = "";
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonBrowse_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                progressBarEnumeration.Visible = false;
+                this.textBoxFilesDirectory.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void buttonEnumerate_Click(object sender, EventArgs e)
+        {
+            if (!ExistsDirectory()) return;
+            var files = Directory.GetFiles(directoryPath);
+            var numFiles = files.Length;
+            SetProgressBar(numFiles);
+
+            for (int i = 1; i <= numFiles; i++)
+            {
+                var file = files[i - 1];
+                Rename(file, numFiles, i);
+                progressBarEnumeration.PerformStep();
+            }
+            MessageBox.Show("Rename successful.");
+        }
+
+        private void SetProgressBar(int length)
+        {
+            progressBarEnumeration.Visible = true;
+            progressBarEnumeration.Minimum = 1;
+            progressBarEnumeration.Maximum = length;
+            progressBarEnumeration.Value = 1;
+            progressBarEnumeration.Step = 1;
+        }
+
+        private bool ExistsDirectory()
+        {
+            if (Directory.Exists(this.textBoxFilesDirectory.Text))
+            {
+                directoryPath = this.textBoxFilesDirectory.Text;
+                return true;
+            }
+            MessageBox.Show("The directory path doesn't exists.", "Error",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            return false;
+        }
+
+        private void Rename(string filePath, int numberOfFiles, int index)
+        {
+            var fileName = Path.GetFileName(filePath);
+            var prefix = GetNumber(numberOfFiles, index);
+            File.Move(filePath, string.Format("{0}/{1}{2}", directoryPath, prefix, fileName));
+        }
+
+        private string GetNumber(int numberOfFiles, int index)
+        {
+            var result = "";
+
+            if (numberOfFiles < 10)
+            {
+                result = string.Format("{0}_", index);
+            }
+            else if (numberOfFiles < 100)
+            {
+                result = string.Format("{0}{1}_", index / 10, index % 10);
+            }
+            else if (numberOfFiles < 1000)
+            {
+                result = string.Format("{0}{1}{2}_", index / 100, (index / 10) % 10, index % 10);
+            }
+            else if (numberOfFiles < 10000)
+            {
+                result = string.Format("{0}{1}{2}{3}_",
+                    index / 1000, (index / 100) % 10, (index / 10) % 10, index % 10);
+            }
+
+            return result;
+        }
+    }
+}
